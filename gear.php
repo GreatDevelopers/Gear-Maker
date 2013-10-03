9441ee20202d1f80322900048ac65724489a4312
gagan <gagan@devil.(none)>
2013-10-04 00:40:15 +0530

    changed layout

--- gear.php
+++ gear.php
@@ -2,11 +2,22 @@
 $PHI = $_POST['pressure-angle'];
 $PC = $_POST['circular-pitch'];
 $teeth = $_POST['teeth'];
-$command = "python gear.py " . $PHI . " " . $PC . " " . $teeth;
+$command = "python process.py " . $PHI . " " . $PC . " " . $teeth;
 exec($command);
 exec('inkscape -z -f file.svg --export-ps=gear.ps');
 exec('pstoedit -f dxf gear.ps  gear.dxf');
 exec('zip gear.zip gear.dxf');
 ?>
-<br><h2><a href = "file.svg" >get svg file here </a></h2>
-<br><h1><a href = "gear.zip" >get dxf here , Autocad format file, in a compressed zip file  :) </a></h1>
+<html>
+<head>
+<title>Gear Design</title>
+</head>
+<body>
+<center>
+<table>
+<td><a href = "file.svg" >get svg file here </a></td>
+<td><a href = "gear.zip" >get dxf here  </a></td>
+</center>
+</table>
+</body>
+</html>
